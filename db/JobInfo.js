f0a1ba57c158644a9377821abe1480ad2fab88f1
arthisolai <137558405+arthisolai@users.noreply.github.com>
2023-08-29 11:38:46 +0200

    jobInfo schema model done

---
+++ db/JobInfo.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const jobInfoSchema = new Schema({
+  company: { type: String, required: true },
+  department: { type: String, required: true },
+  position: { type: String, required: true },
+  location: { type: String, required: true },
+  jobType: { type: String, required: true },
+  description: { type: String, required: true },
+  responsibilities: { type: [String], required: true },
+  qualifications: { type: [String], required: true },
+});
+
+const JobInfo =
+  mongoose.models.JobInfo ||
+  mongoose.model("JobInfo", jobInfoSchema, "JobInfo");
+
+export default JobInfo;
