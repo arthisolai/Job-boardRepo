6831fa89ee9702b5437d183491351e8bde1740d4
arthisolai <137558405+arthisolai@users.noreply.github.com>
2023-09-01 11:53:27 +0200

    finally german calculator is working but not very accuarte

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import PayParityCalculator from "@/Components/PayParityCalculator";
+import TaxCalculator from "@/utils/salaryCalculatorsGermany";
 import useSWR from "swr";
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
   return (
     <>
       <PayParityCalculator />
+      <TaxCalculator />
     </>
   );
 }
