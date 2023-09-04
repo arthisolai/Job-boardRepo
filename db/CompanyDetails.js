2aff194ea947859a46dd5d579c817a9aa1342036
arthisolai <137558405+arthisolai@users.noreply.github.com>
2023-09-04 13:33:14 +0200

    schema for company done

---
+++ db/CompanyDetails.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const companySchema = new Schema({
+  companyName: { type: String, required: true },
+  companyURL: { type: String, required: true },
+  careersURL: { type: String, required: true },
+  aboutCompany: { type: String, required: true },
+  country: { type: String, required: true },
+  city: { type: String, required: true },
+  companySize: { type: String, required: true },
+  industry: { type: String, required: true },
+  foundedIn: { type: Number },
+});
+
+const companyDetails =
+  mongoose.models.CompanyDetails ||
+  mongoose.model("CompanyDetails", companySchema, "CompanyDetails");
+
+export default companyDetails;
