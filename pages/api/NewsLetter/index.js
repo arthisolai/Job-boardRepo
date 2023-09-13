042ec65134b5462726f68890e3ecf52ebf4faa09
arthisolai <137558405+arthisolai@users.noreply.github.com>
2023-09-13 14:59:15 +0200

    checking if scheduled email is being sent

--- pages/api/NewsLetter/index.js
+++ pages/api/NewsLetter/index.js
@@ -1,6 +1,32 @@
-// import Job from "@/db/Job";
-// import dbConnect from "@/db/connect";
+import dbConnect from "@/db/connect";
+import Email from "@/db/Email";
+import { Resend } from "resend";
 
-export default function handler(req, res) {
-  res.status(200).end("Hello cron");
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export default async function handler(req, res) {
+  try {
+    // Connect to the database
+    await dbConnect();
+
+    // Fetch all verified emails
+    const verifiedEmails = await Email.find({ isVerified: true });
+
+    // Loop through each email and send the newsletter or desired content
+    for (let emailDoc of verifiedEmails) {
+      const email = emailDoc.email;
+
+      // Your email sending logic here
+      await resend.emails.send({
+        from: "GetGlobalSupport <support@getglobal.pro>",
+        to: [email],
+        subject: "Your Weekly Newsletter",
+      });
+    }
+
+    res.status(200).end("Emails sent successfully");
+  } catch (error) {
+    console.error("Error sending scheduled emails:", error);
+    res.status(500).end("Internal Server Error");
+  }
 }
